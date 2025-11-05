84bde979560dfa66b3ae19807c10730f41cd3718
Artem Rynochnov <TemiX002@mail.ru>
2025-11-05 15:57:20 +0400

    ➕entity: User ➕repository: UserRepository

---
+++ user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/store/entity/User.kt
@@ -0,0 +1,34 @@
+package com.github.sddisk.usernotes.store.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.NaturalId
+import java.util.UUID
+
+@Entity
+@Table(name = "user_table")
+class User(
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+    val username: String = "",
+    @NaturalId
+    val email: String = "",
+    val password: String = "",
+    val role: Role = Role.USER,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is User) return false
+        return this.email == other.email
+    }
+    override fun hashCode(): Int = email.hashCode()
+    override fun toString(): String = "User(id=$id, username=$username, email=$email, role=$role"
+}
+
+enum class Role {
+    USER, ADMIN
+}
---
+++ user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/store/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.github.sddisk.usernotes.store.repository
+
+import com.github.sddisk.usernotes.store.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserRepository : JpaRepository<User, UUID>

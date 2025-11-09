338a77f2df5b8b2ff3b94e3249a70f5c8c360a56
Artem Rynochnov <TemiX002@mail.ru>
2025-11-09 18:51:28 +0400

    ⚙️ refactor

--- user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/config/security/SecurityConfiguration.kt
+++ user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/config/security/SecurityConfiguration.kt
@@ -3,7 +3,6 @@ package com.github.sddisk.usernotes.config.security
 import com.github.sddisk.usernotes.config.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import java.time.LocalDateTime
 
 @Configuration
 @EnableWebSecurity
--- user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/config/security/jwt/JwtAuthenticationFilter.kt
+++ user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/config/security/jwt/JwtAuthenticationFilter.kt
@@ -5,15 +5,11 @@ import com.github.sddisk.usernotes.service.user.UserService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter

2c7244254c459ead5faaaf03695d6b738757441f
Artem Rynochnov <TemiX002@mail.ru>
2025-11-10 18:51:53 +0400

    Deleted test file and rest-controller

--- user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/config/security/Test.kt
+++
@@ -1,9 +0,0 @@
-package com.github.sddisk.usernotes.config.security
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
-fun main() {
-    val password = "sddisk-is-admin"
-    val encodedPassword = BCryptPasswordEncoder().encode(password)
-    println(encodedPassword)
-}

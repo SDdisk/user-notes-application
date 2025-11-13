20442ee9938e054bf0d2d2b5915353f4b8eaa63f
Artem Rynochnov <TemiX002@mail.ru>
2025-11-13 13:31:46 +0400

    Add exception handler for malformed JSON requests

--- user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/handler/GlobalExceptionHandler.kt
+++ user-notes-backend/src/main/kotlin/com/github/sddisk/usernotes/handler/GlobalExceptionHandler.kt
@@ -6,6 +6,7 @@ import com.github.sddisk.usernotes.exception.UserNotFoundException
 import com.github.sddisk.usernotes.handler.response.ErrorResponse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -88,6 +89,18 @@ class GlobalExceptionHandler {
         )
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpMessageNotReadable(ex: HttpMessageNotReadableException): ErrorResponse {
+        val message = ex.message ?: "Bad JSON format"
+
+        logEx(ex, message)
+
+        return ErrorResponse(
+            message = message,
+            timestamp = LocalDateTime.now()
+        )
+    }
 
     private fun logEx(
         ex: Exception,
